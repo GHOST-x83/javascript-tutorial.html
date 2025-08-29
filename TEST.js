f64f74603e07b6bf2e2ce74f3109b7b1bba0d8aa
iliya divandari <iliyadivandari1388@gmail.com>
2025-08-29 07:37:20 -0600

    اضافه کردن فایل‌های جدید

---
+++ TEST.js
@@ -0,0 +1,11 @@
+//////////////////////////
+// getElementById()
+// انتخاب عنصر با ID خاص
+const myElement = document.getElementById('myId');
+myElement.style.color = 'red';
+////////////////////////////////
+// getElementsByClassName()
+// انتخاب عناصر با کلاس خاص
+const elements = document.getElementsByClassName('myClass');
+elements[0].innerHTML = 'تغییر متن';
+////////////////////////////////////////////////////////
